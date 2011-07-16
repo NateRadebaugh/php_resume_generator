3b3b4e18b129cad342334b4a98f972d00a94352f
Nate Radebaugh <nate.radebaugh@gmail.com>
2011-07-16 01:07:23 -0700

    rewrote resume to be dynamically generated using PHP.
    still need to make it more customizable.
    still need to rewrite css rules
    .
    should get rid of tables.

--- resume.php
+++ resume.php
@@ -1,9 +1,123 @@
+<?php
+$name = ucwords('Nathan T Radebaugh');
+$address = strtoupper('3843 ridgeway dr<br>allison park pa 15101');
+$phone = '412-225-5185';
+$email = strtolower('nradebau@cs.purdue.edu');
+
+$objective = 'To obtain a summer internship position at a company in the field of computer science.';
+$important = 'internship';
+
+$cumulativeGpa = '3.71';
+
+
+
+# skills
+$resumeData['computer skills'][] = 'Windows, Mac, Unix, and Android';
+$resumeData['computer skills'][] = 'HTML, CSS, JavaScript, PHP, and MySQL';
+$resumeData['computer skills'][] = 'Batch, Bash, Ruby, and Perl';
+$resumeData['computer skills'][] = 'C, C++, Java using Object Oriented Programming';
+
+
+
+
+# experience
+$resumeData['experience']['Interim Engineering Intern'] = array(
+	'Qualcomm, Inc.',
+	'San Diego, CA',
+	'May 2011 - August 2011',
+	array(
+		'TD-SCDMA Team',
+		'Wrote tools to automate tests and increase productivity',
+		'Worked on Call Flow Analysis tools'
+	)
+);
+$resumeData['experience']['Senior Computer Consultant'] = array(
+	'Purdue ResNet Services',
+	'West Lafayette, IN',
+	'Fall 2010 - Present',
+	array('Manager')
+);
+$resumeData['experience']['Software Developer'] = array(
+	'Purdue ResNet Services',
+	'West Lafayette, IN',
+	'Spring 2010 - Present',
+	array(
+		'Database Administrator',
+		'Web Developer'
+	)
+);
+$resumeData['experience']['Computer Consultant'] = array(
+	'Purdue ResNet Services',
+	'West Lafayette, IN',
+	'Fall 2009 - Spring 2010'
+);
+$resumeData['experience']['Web Designer'] = array(
+	'The Perfect Pastry',
+	'Allison Park, PA',
+	'2009'
+);
+
+
+# education
+$resumeData['education']['Computer Science'] = array(
+	'Bachelor of Science',
+	'Purdue University',
+	'West Lafayette, IN',
+	'2013'
+);
+$resumeData['education']['Mathematics'] = array(
+	'Minor',
+	'Purdue University',
+	'West Lafayette, IN',
+	'2013'
+);
+$resumeData['education']['Religious Studies'] = array(
+	'Minor',
+	'Purdue University',
+	'West Lafayette, IN',
+	'2013'
+);
+
+
+# related courses
+//$resumeData['related courses'][] = 'Calculus I, II, III. Linear Algebra';
+$resumeData['related courses'][] = 'System Programming';
+$resumeData['related courses'][] = 'Computer Architecture';
+$resumeData['related courses'][] = 'Data Structures and Algorithms';
+$resumeData['related courses'][] = 'Foundations of Computer Science (Discrete Mathematics)';
+$resumeData['related courses'][] = 'Concurrent Programming in Java';
+//$resumeData['related courses'][] = 'Philosophy of Religion, Introduction to the Study of Religion, Religions of the East, Religions of the West';
+
+
+# activities
+$resumeData['activities'][] = array(
+	'Latin and Ballroom Dance Club',
+	'',
+	'Fall 2010 - Spring 2011'
+);
+$resumeData['activities'][] = array(
+	'Purdue Lutheran Ministry',
+	'Vice President of Worship and Service',
+	'Spring 2010 - Fall 2010'
+);
+$resumeData['activities'][] = array(
+	'Fencing Club',
+	'',
+	'Fall 2009'
+);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
 <html style="font-family:Arial"><head><meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
-<title>Resume for Nathan Radebaugh</title>
+<title>Resume for <?php echo $name; ?></title>
 <style type="text/css">
 	table, html {
 		font-size : 90%;
+	}
+	table {
+		width : 100%;
+	}
+	table td {
+		vertical-align : top;
 	}
 	td {
 		padding : 5 0 0 0;
@@ -12,178 +126,105 @@
 	.first {
 		width : 400px !important;
 	}
-	.table td {
-		vertical-align : top;
-	}
-	hr {
-		padding : 0;
-		margin : 5px 0 5px 0;
-	}
-	p {
-		margin : 0;
-		padding : 0;
-	}
-	ul li {
+	p, ul, ul li {
 		margin : 0;
 		padding : 0;
 	}
 	ul {
-		margin : 0;
-		padding : 0;
 		margin-left : 30px;
+	}
+	hr {
+		margin : 5px 0 5px 0;
+		border:1px solid #999;
 	}
 </style>
 </head>
-<body><table style="width:100%"><tbody><tr><td style="text-align:left"><strong><font size="+2">Nathan T Radebaugh</font></strong></td><td style="text-align:right">
+<body>
+<table><tbody>
+<tr><td style="text-align:left;vertical-align:middle"><strong><font size="+2"><?php echo $name; ?></font></strong></td><td style="text-align:right">
 <strong>
-3843 RIDGEWAY DRIVE<br>
-ALLISON PARK PA 15101<br>
-<!--1461 MCCUTCHEON HALL<br>
-400 MCCUTCHEON DRIVE<br>
-WEST LAFAYETTE IN 47906<br>-->
-<!--Hillenbrand Hall, Mailbox #663<br>
-1301 Third Street<br>
-West Lafayette, IN 47906<br>-->
-412-225-5185<br>
-nradebau@cs.purdue.edu</strong></td></tr></tbody></table>
-<hr noshade="">
+<?php echo $address; ?><br />
+<?php echo $phone; ?><br>
+<?php echo $email; ?></strong></td></tr></tbody></table>
+<hr>
 <p><strong>OBJECTIVE</strong></p>
-<p>To obtain a summer <strong>internship</strong> position at a company in the field of computer science.</p>
+<p><?php
+echo str_replace($important,'<strong>'.$important.'</strong>',$objective);
+?></p>
 <center><strong>COMPUTER SKILLS</strong><br></center>
 <ul style="list-style:none">
-<li>Windows, Mac, Unix, and Android.</li>
-<li>HTML, CSS, JavaScript, PHP, MySQL.</li>
-<li>Batch, Bash, Ruby, and Perl.</li>
-<li>C, C++, and Java using Object Oriented Programming.</li>
+<?php
+foreach ($resumeData['computer skills'] as $skill){
+	echo '<li>',$skill,'.</li>',"\n";
+}
+?>
 </ul>
-<hr noshade="">
-<!--
-<center><strong>RELATED ACCOMPLISHMENTS</strong></center>
-<strong>Web Design</strong>
-<table style="width:100%;padding-left:20px;" class="table"><tr>
-<td>Earned eighth place at Technology Student Association international conference.</td><td style="text-align:right">2009</td></tr><tr>
-<td>Earned top fifteen at Technology Student Association international conference.</td><td style="text-align:right">2008</td></tr><tr>
-<td>Earned fourth place at Technology Student Associaion state conference.</td><td style="text-align:right">2007</td></tr><tr>
-<td>Worked with a team alongside MAYA Design, Inc. in the recreation of the firm's Intranet site.</td><td style="text-align:right">2006</td></tr></table>
-</ul>
-<strong>Robotics Programming</strong>
-<table style="width:100%;padding-left:20px;" class="table"><tr><td>Earned top three in the PA/Ohio Regional Botball Robotics Competition as Lead Programmer.</td><td style="text-align:right">2007 - 2009</td></tr><tr>
-<td>Participated in Global Conference on Educational Robotics as Lead Programmer.</td><td style="text-align:right">2008</td></tr></table>
-<hr noshade>-->
+<hr>
 <center><strong>EXPERIENCE</strong><br></center>
-<table style="width:100%" class="table">
-<tbody><tr>
-<td class="first"><strong>Interim Engineering Intern</strong>
-	<ul>
-		<li>TD-SCDMA Team</li>
-		<li>Wrote tools to automate test and increase productivity</li>
-		<li>Worked on Call Flow Analysis tools</li>
-	</ul>
-</td>
-<td>Qualcomm, Inc.<br></td>
-<td>San Diego, CA<br></td>
-<td style="text-align:right">May 2011 - August 2011<br></td>
-</tr><tr>
-<td class="first"><strong>Senior Computer Consultant</strong>
-	<ul>
-		<li>Manager</li>
-	</ul>
-</td>
-<td>Purdue ResNet Services<br></td>
-<td>West Lafayette, IN<br></td>
-<td style="text-align:right">Fall 2010 - Present<br></td>
-</tr>
-<tr>
-<td class="first"><strong>Software Developer</strong>
-	<ul>
-		<li>Database Administrator</li>
-		<li>Web Developer</li>
-	</ul>
-</td>
-<td>Purdue ResNet Services<br></td>
-<td>West Lafayette, IN<br></td>
-<td style="text-align:right">Spring 2010 - Present<br></td>
-</tr>
-<tr>
-<td class="first"><strong>Computer Consultant</strong><br></td>
-<td>Purdue ResNet Services<br></td>
-<td>West Lafayette, IN<br></td>
-<td style="text-align:right">Fall 2009 - Spring 2010<br></td>
-</tr>
-<tr>
-<td class="first"><strong>Web Designer</strong><br></td>
-<td>The Perfect Pastry<br></td>
-<td>Allison Park, PA<br></td>
-<td style="text-align:right">2009<br></td>
-</tr>
-<!--
-<tr>
-<td class="first"><strong>Snow Tubing Lift Attendant</strong><br></td>
-<td>Wildwood Highlands<br></td>
-<td>Wildwood, PA<br></td>
-<td style="text-align:right">2008 - 2009<br></td>
-</tr>
--->
+<table><tbody>
+<?php
+foreach ($resumeData['experience'] as $category => $information){
+	echo
+	'<tr>',"\n",
+	'<td class="first"><strong>',$category,'</strong>',"\n";
+	if (isset($information[3])&&count($information[3])>0){
+		echo '<ul>',"\n";
+		foreach ($information[3] as $description){
+			echo '<li>',$description,'</li>',"\n";
+		}
+		echo '</ul>';
+	}
+	echo
+	'</td>',"\n",
+	'<td>',$information[0],'</td>',
+	'<td>',$information[1],'</td>',
+	'<td style="text-align:right">',$information[2],'</td>',
+	'</tr>';
+}
+?>
 </tbody></table>
-<!--
-<hr noshade="">
-<center><strong>LEADERSHIP</strong></center>
-<table style="width:100%" class="table">
-<tbody><tr>
-<td class="first"><strong>Camp Counselor</strong><br></td>
-<td>Lutherlyn<br></td>
-<td>Butler, PA<br></td>
-<td style="text-align:right">Summer 2010<br></td>
-</tr>
-<tr>
-<td class="first"><strong>Crafts Assistant and Camp Counselor</strong><br></td>
-<td>Lutherlyn<br></td>
-<td>Butler, PA<br></td>
-<td style="text-align:right">Summer 2009<br></td>
-</tr>
-</tbody></table>
--->
-<hr noshade="">
+<hr>
 <center><strong>EDUCATION</strong></center>
-<table style="width:100%" class="table"><tbody><tr><td style="width:90%">
-<strong>Bachelor of Science in <big>Computer Science</big>,</strong> Purdue University, West Lafayette, IN</td><td style="text-align:right">2013</td></tr><tr><td>
-<strong>Minor in <big>Mathematics</big>,</strong> Purdue University, West Lafayette, IN</td><td style="text-align:right">2013</td></tr><tr><td>
-<strong>Minor in <big>Religious Studies</big>,</strong> Purdue University, West Lafayette, IN</td><td style="text-align:right">2013</td></tr><tr><td>
-<strong>Cumulative GPA: <big>3.71 / 4.00</big></strong>
+<table><tbody>
+<?php
+foreach ($resumeData['education'] as $category => $information){
+	echo '<tr>',"\n",
+	'<td style="width:90%">',
+	'<strong>',$information[0],' in <big>',$category,'</big>,</strong> ',
+	$information[1],', ',$information[2],'</td>',
+	'<td style="text-align:right">',$information[3],'</td>',
+	'</tr>';
+}
+?>
+<tr><td>	
+<strong>Cumulative GPA: <big><?php echo $cumulativeGpa; ?> / 4.00</big></strong>
 </td></tr>
 </tbody></table>
-<hr noshade="">
+<hr>
 <center><strong>RELATED COURSES</strong></center>
 <ul style="list-style:none">
-<!--<li>Calculus I, II, III. Linear Algebra.</li>-->
-<li>Systems Programming, <!--<br/>Programming in C, --><br/>Computer Architecture, <br/>Data Structures and Algorithms, <br/>Foundations of Computer Science (Discrete Mathematics), <br/>Concurrent Programming in Java.</li>
-<!--<li>Philosophy of Religion, Introduction to the Study of Religion, Religions of the East, Religions of the West.</li>-->
+<?php
+$numCourses = count($resumeData['related courses']);
+for ($i=0;$i<$numCourses;$i++){
+	echo '<li>',$resumeData['related courses'][$i];
+	echo ($i<($numCourses-1))?',':'.';
+	echo '</li>',"\n";
+}
+?>
 </ul>
-<hr noshade="">
+<hr>
 <center><strong>ACTIVITIES</strong></center>
-<table style="width:100%" class="table">
-<tbody><tr>
-<td><strong>Latin and Ballroom Dance Club</strong><br></td>
-<td>&nbsp;<br></td>
-<td style="text-align:right">Fall 2010-Spring 2011<br></td>
-</tr>
-<tr>
-<td><strong>Purdue Lutheran Ministry</strong><br></td>
-<td>Vice President of Worship and Service<br></td>
-<td style="text-align:right">Spring 2010-Fall 2010<br></td>
-</tr>
-<tr>
-<td><strong>Fencing Club</strong><br></td>
-<td>&nbsp;<br></td>
-<td style="text-align:right">Fall 2009<br></td>
-</tr>
+<table><tbody>
+<?php
+foreach ($resumeData['activities'] as $array){
+	echo
+	'<tr>',"\n",
+		'<td><strong>',$array[0],'</strong></td>',"\n",
+		'<td>',$array[1],'</td>',"\n",
+		'<td style="text-align:right">',$array[2],'</td>',"\n",
+	'</tr>',"\n";
+}
+?>
 </tbody></table>
-<hr noshade="">
-<!--<hr noshade>
-<center><strong>REFERENCES</strong></center>
-<table>
-<tbody>
-<tr><td>Ms. Terry Pronko, Director, Human Resources, MAYA Design, Inc., 412-488-2900.</td></tr>
-<tr><td>Mrs. Mary-Louise Ellena-Wygonik, Enrichment Facilitator, Hampton High School, 412-486-6000 ext. 6387.</td></tr>-->
 </body>
 </html>
