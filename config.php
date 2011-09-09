e2e54070fe0ca89cf118143356ece2fc4008b154
Nate Radebaugh <nate.radebaugh@gmail.com>
2011-09-09 19:48:30 -0400

    Added a config file to decouple.

---
+++ config.php
@@ -0,0 +1,122 @@
+<?php
+$name = 'Nathan T Radebaugh';
+$address = 'Hillenbrand Hall, Mailbox #696<br />
+1301 Third Street Dr.<br />
+West Lafayette, IN 47906';
+$phone = '530-628-3723 (mobile)';
+$email = 'nate.radebaugh@gmail.com';
+$website = "www.naterad.com";
+
+$objective = 'To obtain a summer internship position within the field of software engineering.';
+$important = 'internship';
+
+$cumulativeGpa = '3.71';
+$maximumGpa = '4.00';
+
+
+
+# skills
+$resumeData['computer skills'] = array(
+	'Windows, Mac, Unix, and Android',
+	'SVN, Git, and Perforce',
+	'Batch, Bash, Ruby, and Perl',
+	'HTML, CSS, JavaScript, PHP, and MySQL',
+	'C, C++, and Java using Object Oriented Programming'
+);
+
+
+
+
+# experience
+$resumeData['experience']['Interim Engineering Intern'] = array(
+	'Qualcomm, Inc.',
+	'San Diego, CA',
+	'May 2011 - August 2011',
+	array(
+		'Wrote tools using Batch, Perl, Visual C++ and the MFC framework to automate the running of tests on an external server, generate regression reports, and send out emails, resulting in an over 705% increase in test productivity.',
+		'Wrote PHP and C++ scripts to streamline the creation and maintenance of Call Flow Analysis log parser tools.',
+		'Performed low level structure analysis and alignment, resulting in smaller data packets and faster data transfer speeds.',
+		'Wrote Ruby scripts to analyze header files and automatically refactor and decouple header files, significantly decreasing compile times.'
+	)
+);
+$resumeData['experience']['Senior Computer Consultant'] = array(
+	'Purdue ResNet',
+	'West Lafayette, IN',
+	'Fall 2010 - Present',
+	array(
+		'Supervised ResNet Employees',
+		'Active role in the interviewing, hiring, and training of new employees'
+	)
+);
+$resumeData['experience']['Software Developer'] = array(
+	'Purdue ResNet',
+	'West Lafayette, IN',
+	'Spring 2010 - Present',
+	array(
+		'Led a team of eight student developers in the development and maintenance of a PHP/MySQL driven management tool using the open source PHP Framework FuelPHP.',
+		'Developed PHP and Perl scripts to help monitor usage across the network.',
+		'Administered multiple Linux web and database servers used for development and production environments.'
+	)
+);
+$resumeData['experience']['Computer Consultant'] = array(
+	'Purdue ResNet',
+	'West Lafayette, IN',
+	'Fall 2009 - Spring 2010',
+	array(
+		'Provided telephone and onsite support for over 10,000 ResNet internet users'
+	)
+);
+$resumeData['experience']['Web Designer'] = array(
+	'The Perfect Pastry',
+	'Allison Park, PA',
+	'2009',
+	array(
+		'Worked with a client to create a website for a local bakery using HTML, CSS, JavaScript, and PHP.'
+	)
+);
+
+
+# education
+$resumeData['education']['Computer Science, concentration in Software Engineering and Programming Languages'] = array(
+	'B.S.',
+	'Purdue University',
+	'West Lafayette, IN',
+	'May 2013 (expected)'
+);
+$resumeData['education']['Mathematics'] = array(
+	'Minor'
+);
+$resumeData['education']['Religious Studies'] = array(
+	'Minor'
+);
+
+
+# related courses
+$resumeData['related courses'] = array(
+	'Operating Systems',
+	'Software Engineering',
+	'Web Information Retrieval and Management',
+	'Systems Programming : A',
+	'Computer Architecture : B',
+	'Data Structures and Algorithms : A-',
+	'Foundations of Computer Science (Discrete Mathematics) : B',
+	'Concurrent Programming in Java : A'
+);
+
+
+# activities
+$resumeData['activities'][] = array(
+	'Latin and Ballroom Dance Club',
+	'',
+	'Fall 2010 - Spring 2011'
+);
+$resumeData['activities'][] = array(
+	'Purdue Lutheran Ministry',
+	'VP of Worship and Service',
+	'Spring 2010 - Present'
+);
+$resumeData['activities'][] = array(
+	'Fencing Club',
+	'',
+	'Fall 2009'
+);
--- resume.php
+++ resume.php
@@ -1,127 +1,6 @@
 <?php
 ob_start();
-
-$name = 'Nathan T Radebaugh';
-$address = 'Hillenbrand Hall, Mailbox #696<br />
-1301 Third Street Dr.<br />
-West Lafayette, IN 47906';
-$phone = '530-628-3723 (mobile)';
-$email = 'nate.radebaugh@gmail.com';
-$website = "www.naterad.com";
-
-$objective = 'To obtain a summer internship position within the field of software engineering.';
-$important = 'internship';
-
-$cumulativeGpa = '3.71';
-$maximumGpa = '4.00';
-
-
-
-# skills
-$resumeData['computer skills'] = array(
-	'Windows, Mac, Unix, and Android',
-	'SVN, Git, and Perforce',
-	'Batch, Bash, Ruby, and Perl',
-	'HTML, CSS, JavaScript, PHP, and MySQL',
-	'C, C++, and Java using Object Oriented Programming'
-);
-
-
-
-
-# experience
-$resumeData['experience']['Interim Engineering Intern'] = array(
-	'Qualcomm, Inc.',
-	'San Diego, CA',
-	'May 2011 - August 2011',
-	array(
-		'Wrote tools using Batch, Perl, Visual C++ and the MFC framework to automate the running of tests on an external server, generate regression reports, and send out emails, resulting in an over 705% increase in test productivity.',
-		'Wrote PHP and C++ scripts to streamline the creation and maintenance of Call Flow Analysis log parser tools.',
-		'Performed low level structure analysis and alignment, resulting in smaller data packets and faster data transfer speeds.',
-		'Wrote Ruby scripts to analyze header files and automatically refactor and decouple header files, significantly decreasing compile times.'
-	)
-);
-$resumeData['experience']['Senior Computer Consultant'] = array(
-	'Purdue ResNet',
-	'West Lafayette, IN',
-	'Fall 2010 - Present',
-	array(
-		'Supervised ResNet Employees',
-		'Active role in the interviewing, hiring, and training of new employees'
-	)
-);
-$resumeData['experience']['Software Developer'] = array(
-	'Purdue ResNet',
-	'West Lafayette, IN',
-	'Spring 2010 - Present',
-	array(
-		'Led a team of eight student developers in the development and maintenance of a PHP/MySQL driven management tool using the open source PHP Framework FuelPHP.',
-		'Developed PHP and Perl scripts to help monitor usage across the network.',
-		'Administered multiple Linux web and database servers used for development and production environments.'
-	)
-);
-$resumeData['experience']['Computer Consultant'] = array(
-	'Purdue ResNet',
-	'West Lafayette, IN',
-	'Fall 2009 - Spring 2010',
-	array(
-		'Provided telephone and onsite support for over 10,000 ResNet internet users'
-	)
-);
-$resumeData['experience']['Web Designer'] = array(
-	'The Perfect Pastry',
-	'Allison Park, PA',
-	'2009',
-	array(
-		'Worked with a client to create a website for a local bakery using HTML, CSS, JavaScript, and PHP.'
-	)
-);
-
-
-# education
-$resumeData['education']['Computer Science, concentration in Software Engineering and Programming Languages'] = array(
-	'B.S.',
-	'Purdue University',
-	'West Lafayette, IN',
-	'May 2013 (expected)'
-);
-$resumeData['education']['Mathematics'] = array(
-	'Minor'
-);
-$resumeData['education']['Religious Studies'] = array(
-	'Minor'
-);
-
-
-# related courses
-$resumeData['related courses'] = array(
-	'Operating Systems',
-	'Software Engineering',
-	'Web Information Retrieval and Management',
-	'Systems Programming : A',
-	'Computer Architecture : B',
-	'Data Structures and Algorithms : A-',
-	'Foundations of Computer Science (Discrete Mathematics) : B',
-	'Concurrent Programming in Java : A'
-);
-
-
-# activities
-$resumeData['activities'][] = array(
-	'Latin and Ballroom Dance Club',
-	'',
-	'Fall 2010 - Spring 2011'
-);
-$resumeData['activities'][] = array(
-	'Purdue Lutheran Ministry',
-	'VP of Worship and Service',
-	'Spring 2010 - Present'
-);
-$resumeData['activities'][] = array(
-	'Fencing Club',
-	'',
-	'Fall 2009'
-);
+include("config.php");
 ?>
 
 
@@ -208,7 +87,7 @@ $resumeData['activities'][] = array(
 	
 	<div class="label">Objective:</div>
 	<div class="content">
-	<?php echo str_replace($important,'<strong class="highlight">'.$important.'</strong>',$objective); ?>
+	<?php echo str_replace($important,'<strong>'.$important.'</strong>',$objective); ?>
 	</div>
 	<div class="label">Software Skills:</div>
 	<div class="content">
@@ -242,6 +121,7 @@ $resumeData['activities'][] = array(
 	}
 	?>
 	</div>
+<?php if (count($resumeData) > 0){ ?>
 	<div class="label">Related Courses:</div>
 	<div class="content">
 	<table><tbody>
@@ -262,8 +142,13 @@ $resumeData['activities'][] = array(
 	}
 	?>
 	</tbody></table>
+<?php if ( empty($cumulativeGpa) === false && empty($maximumGpa) === false ){ ?>
 	<strong style="display:inline-block;margin-top:0.25em">Cumulative GPA: <?php echo $cumulativeGpa; ?> / <?php echo $maximumGpa; ?></strong>
+<?php } ?>
 	</div>
+<?php } ?>
+
+<?php if ( count($resumeData['experience']) > 0){ ?>
 	<div class="label">Work Experience:</div>
 	<div class="content">
 	<?php foreach ($resumeData['experience'] as $category => $information)
@@ -284,6 +169,8 @@ $resumeData['activities'][] = array(
 		</tr>
 	<?php } ?>
 	</div>
+<?php } ?>
+<?php if ( count($resumeData['activities']) > 0){ ?>
 	<div class="label">Activities:</div>
 	<div class="content">
 	<table><tbody>      
@@ -304,6 +191,7 @@ $resumeData['activities'][] = array(
 	?>
 	</tbody></table>
 	</div>
+<?php } ?>
 </div>
 </body>
 </html>
